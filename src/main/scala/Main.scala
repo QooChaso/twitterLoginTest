b0684529385b30b9836ea1347fb9a7a1f957ea9d
QooChaso <qoochaso238@gmail.com>
2019-05-10 13:43:30 +0900

    simple request sample

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,31 @@
-object Main extends App{
 
+import http.HttpClient
+
+object Main extends App{
+  val client = new HttpClient
+  val header = Map{
+    "Cache-Control" -> "public"
+    "Content-Length" -> "219"
+    "Content-Type" -> "text/html;"
+    "Server" -> "gws"
+    "X-Frame-Options" ->  "SAMEORIGIN"
+    "X-XSS-Protection" -> "0"
+  }
+
+  val response = client.Get("http://google.com", header)
+  print(response)
 }
+
+
+//Google.comのheader内容
+/*
+Cache-Control: public, max-age=2592000
+Content-Length: 219
+Content-Type: text/html; charset=UTF-8
+Date: Fri, 10 May 2019 04:12:33 GMT
+Expires: Sun, 09 Jun 2019 04:12:33 GMT
+Location: http://www.google.com/
+Server: gws
+X-Frame-Options: SAMEORIGIN
+X-XSS-Protection: 0
+ */
---
+++ src/main/scala/http/CommonHttpClient.scala
@@ -0,0 +1,18 @@
+package http
+
+trait WebMethod {
+  def Get(url: String, headers: Map[String, String]): CustomResponse
+
+  def Post(url: String, headers: Map[String, String], requestBody: String): CustomResponse
+
+  def Put(url: String, headers: Map[String, String], requestBody: String): CustomResponse
+
+  def Delete(url: String, headers: Map[String, String]): CustomResponse
+}
+
+abstract class CommonHttpClient extends WebMethod {
+  val readTimeoutMillis = 5000
+  var connectTimeoutMillis = 5000
+}
+
+case class CustomResponse(statusCode: Int, status: String, body: String)
