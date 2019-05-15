3025454d29aa2e9cda427790794fa7c19a8adf47
QooChaso <qoochaso238@gmail.com>
2019-05-15 13:36:30 +0900

    add typesafe config

--- build.sbt
+++ build.sbt
@@ -7,4 +7,7 @@ scalaVersion := "2.12.8"
 libraryDependencies ++= Seq(
   //sttp
   "com.softwaremill.sttp" %% "core" % "1.5.11",
+
+  //typesafe.config
+  "com.typesafe" % "config" % "1.3.1"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,31 +1,20 @@
 
 import http.HttpClient
+import com.typesafe.config.ConfigFactory
 
 object Main extends App{
   val client = new HttpClient
-  val header = Map{
-    "Cache-Control" -> "public"
-    "Content-Length" -> "219"
-    "Content-Type" -> "text/html;"
-    "Server" -> "gws"
-    "X-Frame-Options" ->  "SAMEORIGIN"
-    "X-XSS-Protection" -> "0"
+  val conf = ConfigFactory.load()
+
+  val twitterHomeHeader = Map{
+    "cache-control" -> "max-age=0"
+    "content-type" -> "application/x-www-form-urlencoded"
+    "dnt" -> "1"
+    "referer" -> "https://twitter.com/"
+    "upgrade-insecure-requests" -> "1"
+    "user-agent" -> "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36"
   }
 
-  val response = client.Get("http://google.com", header)
-  print(response)
+  val response = client.Get(conf.getString(""), twitterHomeHeader)
+  print(response.body)
 }
-
-
-//Google.comのheader内容
-/*
-Cache-Control: public, max-age=2592000
-Content-Length: 219
-Content-Type: text/html; charset=UTF-8
-Date: Fri, 10 May 2019 04:12:33 GMT
-Expires: Sun, 09 Jun 2019 04:12:33 GMT
-Location: http://www.google.com/
-Server: gws
-X-Frame-Options: SAMEORIGIN
-X-XSS-Protection: 0
- */
